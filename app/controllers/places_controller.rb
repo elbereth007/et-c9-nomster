0230b9d78cbaff393902dc49b951ca4ab5c06658
Victor Christensen <victor.christensen@cox.net>
2017-05-20 03:30:49 +0000

    firehose track 3, lesson 4

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,5 @@
+# file created 19 may 17
+class PlacesController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # next line added 19 may 17
+  root 'places#index'
 end

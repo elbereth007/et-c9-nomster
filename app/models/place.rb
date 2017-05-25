90600f21eeafa06d6816420bb38dccb22bb32233
Victor Christensen <victor.christensen@cox.net>
2017-05-25 01:02:37 +0000

    firehose track 3, lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ApplicationRecord
 
 # next line added 22 may 17
     belongs_to :user
-# next line added 24 may 17; ensure no one enters blank name for place
-    validates :name, presence: true
+# next 2 line added 24 may 17; ensure no one enters blanks for any field, name > 3 chars
+    validates :name, presence: true, length: {minimum: 4}
+    validates :address, :description, presence: true
 end

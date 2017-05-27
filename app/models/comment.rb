2b8d4c5ab1242369498d833981b2dd8771ef7a68
Victor Christensen <victor.christensen@cox.net>
2017-05-27 03:10:54 +0000

    firehose track 3, lesson 35

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ApplicationRecord
 # next 2 lines added 26 may 17 - connects comment with a user and place
     belongs_to :user
     belongs_to :place
+    
+# next 10 lines added 26 may 17 - to add dropdown for star ratings
+    RATINGS = {
+        'one star': '1_star',
+        'two stars': '2_stars',
+        'three stars': '3_stars',
+        'four stars': '4_stars',
+        'five stars': '5_stars'
+    }
+    
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
 end

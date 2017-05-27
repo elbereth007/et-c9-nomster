021d191aa5addfee8492024c30a5857aa69f5e62
Victor Christensen <victor.christensen@cox.net>
2017-05-27 00:29:32 +0000

    firehose track 3, lesson 33

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # next line added 19 may 17
   root 'places#index'
-  # next line added 22 may 17
-  resources :places
+  # next line added 22 may 17, "do" added 26 may 17
+  resources :places do
+ # next 2 lines added 26 may 17
+    resources :comments, only: :create
+  end
 end
